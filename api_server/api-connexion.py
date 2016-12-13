e47b852d4edbe9e65a2a4a8ba37f7c91250dd9aa
Abner G Jacobsen <admin@daspanel.com>
2016-12-13 21:59:06 -0200

    Set api-connexion.py as a default app.

--- api_server/api-connexion.py
+++ api_server/api-connexion.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""API Server
+
+Daspanel API server
+
+ :copyright: (c) 2016, Abner G Jacobsen.
+             All rights reserved.
+ :license:   GNU General Public License v3, see LICENSE for more details.
+
+"""
 from __future__ import absolute_import, division, print_function
+from __about__ import *
 import os
 import connexion
 import datetime
 import logging
 
-from raven.contrib.flask import Sentry
-
 import config as CONFIG
 
-logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
+# Create Connexion app
 app = connexion.App(__name__)
 
+# Add api blueprints
 app.add_api('swagger/apiserver.yaml')
-#app.add_api('swagger/databases.yaml', base_path='/1.0/databases')
-#app.add_api('swagger/domains.yaml', base_path='/1.0/domains')
 app.add_api('swagger/sites.yaml', base_path='/1.0/sites')
 
+# Trick: app.app is the Flask app object inside Connexion app object
+flask_app = app.app
+flask_app.logger_name = "daspanel_api_server"
 
-# set the WSGI application callable to allow using uWSGI:
-# uwsgi --http :8080 -w app
-
-# app.app is the Flask app object
-application = app.app
-sentry = Sentry(application, dsn='https://90ba14f0f5f947369379a2eb2e63391e:6c32e551d4104dd99287e16200218d24@sentry.io/98530')
-
-if __name__ == '__main__':
-
-    # run our standalone gevent server
-    app.run(host='0.0.0.0', port=8080)
