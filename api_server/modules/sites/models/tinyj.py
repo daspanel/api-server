7128c374af1aaa57d3829ee7867e021fb4363132
Abner G Jacobsen <abner@apoana.com.br>
2017-11-20 19:36:26 -0200

    Fixed bug that prevented the correct operation of the _last_update and _created_at fields. Added another default field: _fmt_version

--- api_server/modules/sites/models/tinyj.py
+++ api_server/modules/sites/models/tinyj.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Configuration models."""
+"""Sites models."""
 from __future__ import absolute_import, division, print_function
 import os
 from datetime import datetime
@@ -10,9 +10,11 @@ from jsonmodels import models, validators
 
 dbucket = os.environ['DASPANEL_SYS_UUID']
 DASPANEL_DATABASESFILE = '/opt/daspanel/data/' + dbucket + '/db/daspanel-sites.json'
+SITES_DB_FMT = '0.1.0'
 
 # Open sites database, creating it if not exists
 SITES_DB = Database(DASPANEL_DATABASESFILE)
+
 
 class SiteVersion(TinyJsonModel):
     __tablename__ = "siteversions"
@@ -31,25 +33,28 @@ class SiteRedirects(TinyJsonModel):
     ssl = fields.StringField(required=True, validators=[validators.Length(1, 32)])
     version = fields.StringField(required=True, validators=[validators.Length(25, 25)])
 
+
 class SiteModel(TinyJsonModel):
     __tablename__ = "sites"
     urlprefix = fields.StringField(required=True, validators=[validators.Length(1, 255)])
-    #sitetype = fields.StringField(required=True, validators=[validators.Length(1, 64)])
     sitedescription = fields.StringField(required=True, validators=[validators.Length(1, 255)])
-    #runtime = fields.StringField(required=True, validators=[validators.Length(1, 64)])
     versions = fields.ListField(['SiteVersion'])
     redirects = fields.ListField(['SiteRedirects'])
     active_version = fields.StringField(required=True, validators=[validators.Length(25, 25)])
     active_dir = fields.StringField(required=True, validators=[validators.Length(1, 255)])
     _last_update = fields.DateTimeField(required=True)
     _created_at = fields.DateTimeField(required=True)
+    _fmt_version = fields.StringField(required=False)
 
-    def __init__(self, *args, **kwargs):
-        super(SiteModel, self).__init__(*args, **kwargs)
+    def save(self):
+        self._last_update = datetime.utcnow()
+        return super(self.__class__, self).save()
+
+    def insert(self):
         self._last_update = datetime.utcnow()
         self._created_at = self._last_update
-        #if 'db' in kwargs:
-        #    self.Meta.database = kwargs['db']
+        self._fmt_version = SITES_DB_FMT
+        return super(self.__class__, self).insert()
 
     def version_exist(self, version):
         records = self.to_struct()['versions']
@@ -91,7 +96,7 @@ class SiteModel(TinyJsonModel):
             if rec['urlprefix'] == prefix:
                 return True, rec['_cuid']
         return False, None
-        
+
     class Meta:
         database = SITES_DB
 
